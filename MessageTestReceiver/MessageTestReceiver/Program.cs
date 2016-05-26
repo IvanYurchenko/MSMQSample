82842fa161bb993909a880cbd44bdb004d96cb2b
Ivan Yurchenko <Ivan Yurchenko>
2016-05-26 19:51:43 +0300

    Initial commit

---
+++ MessageTestReceiver/MessageTestReceiver/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Messaging;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace MessageTestReceiver
+{
+    public class Program
+    {
+        private const string QueueName = ".\\Private$\\MyProgram";
+
+        public class MyMessage
+        {
+            public string Command { get; set; }
+            public int EntityID { get; set; }
+        }
+
+        private static MyMessage RecieveMessage()
+        {
+            if (!MessageQueue.Exists(QueueName))
+            {
+                return null;
+            }
+
+            using (var msmq = new MessageQueue(QueueName))
+            {
+                msmq.Formatter = new XmlMessageFormatter(new Type[] { typeof(MyMessage) });
+                var message = msmq.Receive();
+                return message != null && message.Body is MyMessage ? (MyMessage)message.Body : null;
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            var t2 = new Task(() =>
+            {
+                while (true)
+                {
+                    var msg = RecieveMessage();
+                    while (msg != null)
+                    {
+                        Console.WriteLine("Message received: " + msg.Command + " " + msg.EntityID);
+                        msg = RecieveMessage();
+                    }
+
+                    Thread.Sleep(1000);
+                }
+            });
+
+            t2.Start();
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ MessagingTest/MessagingTest/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Messaging;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace MessagingTest
+{
+    public class MyMessage
+    {
+        public string Command { get; set; }
+        public int EntityID { get; set; }
+    }
+
+    class Program
+    {
+        private const string MessageAddress = @".\Private$\MyProgram";
+        private const string QueueName = ".\\Private$\\MyProgram";
+
+        private static void SendMessage(string str, int x)
+        {
+            using (var msmq = new MessageQueue(MessageAddress))
+                msmq.Send(new MyMessage
+                {
+                    Command = str,
+                    EntityID = x
+                });
+        }
+
+        static void Main(string[] args)
+        {
+            try
+            {
+                if (!MessageQueue.Exists(QueueName))
+                {
+                    MessageQueue.Create(QueueName);
+                }
+
+                var t = new Task(() =>
+                {
+                    var i = 0;
+                    while (true)
+                    {
+                        SendMessage("hello", i);
+                        Console.WriteLine("Message sent. Message number: " + i);
+                        i++;
+                        Thread.Sleep(1500);
+                    }
+                });
+
+                t.Start();
+
+                Console.ReadKey();
+            }
+            finally
+            {
+                MessageQueue.Delete(QueueName);
+            }
+        }
+    }
+}
